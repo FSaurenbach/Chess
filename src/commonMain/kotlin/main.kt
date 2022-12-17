111b26412803a041b9e4751ddd000217daf16e3f
FSaurenbach <ferdinand161@outlook.de>
2022-12-17 16:35:09 +0100

    First Logic of Player Movement

--- src/commonMain/kotlin/main.kt
+++ src/commonMain/kotlin/main.kt
@@ -1,55 +1,48 @@
-import com.soywiz.klock.*
-import com.soywiz.klogger.*
-import com.soywiz.korev.*
-import com.soywiz.korev.DummyEventDispatcher.addEventListener
 import com.soywiz.korge.*
 import com.soywiz.korge.input.*
-import com.soywiz.korge.render.*
 import com.soywiz.korge.scene.*
-import com.soywiz.korge.tween.*
-import com.soywiz.korge.ui.*
 import com.soywiz.korge.view.*
 import com.soywiz.korim.color.*
-import com.soywiz.korim.format.*
-import com.soywiz.korio.file.std.*
-import com.soywiz.korma.geom.*
-import com.soywiz.korma.interpolation.*
-import kotlin.math.*
-val sizeItem: Double = min(512.0, 512.0) / 8.0
+
+const val fieldSize: Double = 512.0 / 8.0
 suspend fun main() = Korge(width = 512, height = 512, bgcolor = Colors["#2b2b2b"]) {
 	val sceneContainer = sceneContainer()
 
-	sceneContainer.changeTo({ MyScene() })
+	sceneContainer.changeTo({ MyScene(sceneContainer) })
 }
 
-class MyScene : Scene() {
+class MyScene(private var scontainer: Any) : Scene() {
 	override suspend fun SContainer.sceneMain() {
-        val board = addChild(Board())
-        addChild(Piece("Pawn","White", 0, 1, board))
-
-
+        addChild(Board())
+        //addChild(Piece("Pawn","White", 0, 1, scontainer))
+        var rpiece = Piece("Pawn","White", 1, 3, scontainer).addTo(this)
+        rpiece.changePosition(3, 3)
 	}
 }
-class Piece(val type:String, var Color:String, var PieceX:Int, var PieceY:Int, var board: Unit) :Container() {
+class Piece(val type:String, var Color:String, var pieceX:Int, var pieceY:Int, var scontainer: Any) :Container() {
 
     init {
-        val piece = solidRect(sizeItem/2, sizeItem/2, Colors.RED)
-        piece.xy(x*50, y*50)
+        val piece = solidRect(fieldSize / 2, fieldSize / 2, Colors.RED)
+        piece.xy(pieceX*64, pieceY*64)
         addChild(piece)
         centerPiece(piece)
         move(piece)
     }
-    private fun centerPiece(piece:SolidRect) {
+    fun changePosition(x:Int, y:Int) {
+        piece.xy(x*64, y*64)
+    }
+    private fun centerPiece(piece: SolidRect) {
         piece.onCollision() {
-            if (it.name == "Board"){
-                var u =5
-                //piece.centerOn(it)
+            if (it.name == "Board") {
+                piece.centerOn(it)
             }
 
         }
     }
-    private fun move(piece:SolidRect) {
-        var clicked = false
+
+    private fun move(piece: SolidRect) {
+
+        var clicked = true
         piece.onClick {
             println(clicked)
             if (!clicked) {
@@ -57,13 +50,24 @@ class Piece(val type:String, var Color:String, var PieceX:Int, var PieceY:Int, v
                 println("clicked")
             }
 
+
         }
-        
 
+
+
+        this.onUpAnywhere {
+            if (clicked) {
+                piece.pos = this.mouse.currentPosLocal
+                centerPiece(piece)
+                println("up")
+                clicked = false
+            }
+
+
+        }
     }
+
 }
-
-
 class Board() :Container() {
 
     private val white = Colors["#f0d9b5"]
@@ -74,22 +78,22 @@ class Board() :Container() {
 
         for (i in 0..7)
             for (j in 0..7) {
-                val rect = SolidRect(sizeItem, sizeItem, if ((j + i) % 2 != 0) black else white)
-                rect.xy(i * sizeItem, j * sizeItem)
+                val rect = SolidRect(fieldSize, fieldSize, if ((j + i) % 2 != 0) black else white)
+                rect.xy(i * fieldSize, j * fieldSize)
                 rect.name = "Board"
                 addChild(rect)
                 rectsBoard[i to j] = rect
             }
         for (i in 0..7) {
             text("${8 - i}", textSize = 25.0, color = if (i % 2 != 0) black else white)
-                .xy(8 * sizeItem - 15.0, 1 + i * sizeItem)
+                .xy(8 * fieldSize - 15.0, 1 + i * fieldSize)
                 .addTo(this)
         }
         val letters = "abcdefgh"
         for (i in 0..7) {
             text(letters[i] + "", textSize = 25.0, color = if (i % 2 != 0) black else white)
 
-                .xy(3 + i * sizeItem, 8 * sizeItem - 30.0)
+                .xy(3 + i * fieldSize, 8 * fieldSize - 30.0)
                 .addTo(this)
         }
 
